58853e45ea4ebb5dfecc47c663b2712d752a813b
Anthony Akentiev <anthony.akentiev@gmail.com>
2014-02-22 12:29:09 +0400

    Little bit of changes for lesson 33

--- codeschool/angular/thirty-three/api-response.js
+++ codeschool/angular/thirty-three/api-response.js
@@ -1,12 +1,16 @@
 angular.module('publicapi.transformers').factory('services.transformer.ApiResponse',function(){
 	return function(data){
           data = JSON.parse(data);
+
           if(data.length)
           {
                data = _.map(data,function(repo){
                     return {userName:repo.full_name, gitUrl: repo.git_url};
                });
           }
-		return data;
+
+          console.log('-->Data: ');
+          console.log(data);
+          return data;
 	}
 });
